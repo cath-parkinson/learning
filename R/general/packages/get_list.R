7010c8ee50ccfa1ff6d70ecf4d63faec9649d6ca
Cath <catherine.parkinson@brightblueconsulting.co.uk>
2022-01-17 13:39:38 +0000

    extract and save down list of installed packages plus version

---
+++ R/general/packages/get_list.R
@@ -0,0 +1,34 @@
+# get list of installed packages
+library(tidyverse)
+
+package_df <- installed.packages() %>% 
+  as.tibble()
+
+package_df <- package_df %>% filter(is.na(Priority))
+
+make_package_list <- function(package_df){
+  
+  package_list <- list()
+  
+  for (i in 1:nrow(package_df)){
+    
+    package_list[[i]] <- package_df %>% pull(Version) %>% nth(i)
+    
+  }
+  
+  names(package_list) <- package_df %>% pull(Package)
+  return(package_list)
+  
+}
+
+package_list <- make_package_list(package_df)
+
+# get in format needed for passing to renv.lock file function
+test <- list(digest = "0.6.22")
+
+# save ready to use in different project
+# pass list to the renv::record() function
+
+saveRDS(package_list, file = "package_list_learning_170122.RData")
+package_list_check <- readRDS("package_list_learning_170122.RData")
+
