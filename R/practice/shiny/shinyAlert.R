3e7c18f94c79a5f9d2ddc9f2b9a468fbbe33250f
Cath <catherine.parkinson@brightblueconsulting.co.uk>
2022-03-02 20:09:51 +0000

    Functionalise the shinyalert

--- R/practice/shiny/shinyAlert.R
+++ R/practice/shiny/shinyAlert.R
@@ -2,9 +2,59 @@
 library(shiny)
 library(shinyalert)
 
+# Customise our own shiny alert
+
+#' @param myinputId name the input - namespace if neccesary
+#' @param mytitle  title of alert
+#' @param mytext "" or "name-desc" or "desc-inputname"
+#'
+#' @return A shiny alert function.
+mm_shinyalert <- function(myinputId,
+                            mytitle,
+                            mytext = c(""),
+                            myconfirmButtonText = "ENTER",
+                            confirmButtonCol = "#28b78d"){
+  
+  if(mytext == "name-desc") { tagList(HTML(paste(strong("PIPE IN NAME:"), "pipe in desc"))) }
+  
+  if(mytext == "desc-inputname") { tagList(
+    
+    HTML(
+      
+      paste(strong("Description:"), "pipe in desc")),
+    
+    textInput(inputId = "user_name_scenario",
+              label = "",
+              width = "100%",
+              placeholder = "Give your scenario a name")) }
+  
+  shinyalert(inputId = myinputId,
+             title = mytitle,
+             text = mytext,
+             type = "",
+             confirmButtonText = myconfirmButtonText,
+             cancelButtonText = "CANCEL",
+             showCancelButton = TRUE,
+             showConfirmButton = TRUE,
+             confirmButtonCol = "#001D38",
+             html = TRUE)
+  
+}
+
+
+
 ui <- fluidPage(
   actionButton(inputId = "button",
-               label = "GO")
+               label = "GO"),
+  actionButton(inputId = "button_save",
+               label = "SAVE"),
+  actionButton(inputId = "button_save_as",
+               label = "SAVE AS"),
+  actionButton(inputId = "button_new",
+               label = "NEW"),
+  actionButton(inputId = "mybutton",
+               label = "TEST")
+  
 )
 server <- function(input, output, session) {
   
@@ -12,7 +62,7 @@ server <- function(input, output, session) {
     
     shinyalert(inputId = "mmm_run_user_confirm",
                title = "SCENARIO LOADED",
-               text = tagList(HTML(paste(strong("NEW:"), "pipe in desc"))), 
+               text = tagList(HTML(paste(strong("PIPE IN NAME:"), "pipe in desc"))), 
                type = "",
                confirmButtonText = "RUN",
                cancelButtonText = "CANCEL",
@@ -22,6 +72,71 @@ server <- function(input, output, session) {
                html = TRUE)
     
   })
+  
+  observeEvent(input$button_save, {
+    
+    shinyalert(inputId = "mmm_run_user_confirm",
+               title = "SAVE SCENARIO",
+               text = tagList(HTML(paste(strong("PIPE IN NAME:"), "pipe in desc"))), 
+               type = "",
+               confirmButtonText = "SAVE",
+               cancelButtonText = "CANCEL",
+               showCancelButton = TRUE,
+               showConfirmButton = TRUE,
+               confirmButtonCol = "#001D38",
+               html = TRUE)
+    
+  })
+  
+  
+  observeEvent(input$button_save_as, {
+    
+    shinyalert(inputId = "mmm_run_user_confirm",
+               title = "SAVE SCENARIO",
+               text = tagList(
+                 
+                 HTML(
+                 
+                 paste(strong("Description:"), "pipe in desc")),
+                 
+                 textInput(inputId = "user_name_scenario",
+                           label = "",
+                           width = "100%",
+                           placeholder = "Give your scenario a name")),
+               type = "",
+               confirmButtonText = "SAVE",
+               cancelButtonText = "CANCEL",
+               showCancelButton = TRUE,
+               showConfirmButton = TRUE,
+               confirmButtonCol = "#001D38",
+               html = TRUE)
+    
+  })
+  
+  
+  observeEvent(input$button_new, {
+    
+    shinyalert(inputId = "mmm_run_user_confirm",
+               title = "NEW SCENARIO",
+               text = "",
+               type = "",
+               confirmButtonText = "CREATE",
+               cancelButtonText = "CANCEL",
+               showCancelButton = TRUE,
+               showConfirmButton = TRUE,
+               confirmButtonCol = "#001D38",
+               html = TRUE)
+    
+  })
+  
+  observeEvent(input$mybutton, {
+    
+    reop_shinyalert(myinputId = "mynewbutton",
+                    mytitle = "MY TEST",
+                    mytext = "")
+      })
+  
+  
   
 }
 shinyApp(ui, server)
