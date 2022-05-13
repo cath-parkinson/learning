2451f3982833fcc7552b34225a6571f7eae545cb
Cath <catherine.parkinson@brightblueconsulting.co.uk>
2022-05-13 16:39:20 +0100

    Update renv version

--- R/general/packages/get_list.R
+++ R/general/packages/get_list.R
@@ -1,6 +1,8 @@
 # get list of installed packages
 library(tidyverse)
 
+# LIST ----------------------------------
+
 # package_df <- installed.packages() %>% 
 #   as.tibble()
 # 
@@ -13,8 +15,9 @@ make_package_list <- function(){
   
   # remove base R packages and INLA
   package_df <- package_df %>% 
-    filter(is.na(Priority)) %>% 
-    filter(!Package == "INLA")
+    filter(is.na(Priority)) 
+  # %>% 
+    # filter(!Package == "INLA")
   
   package_list <- list()
   
@@ -37,6 +40,26 @@ test <- list(digest = "0.6.22")
 # save ready to use in different project
 # pass list to the renv::record() function
 
-saveRDS(package_list, file = "package_list_learning_170122.RData")
-package_list_check <- readRDS("package_list_learning_170122.RData")
+saveRDS(package_list, file = "package_list_mt_branch_398_110422.RData")
+package_list_check <- readRDS("package_list_mt_branch_398_110422.RData")
+
+# DF -----------------------------------------
+
+make_package_df <- function(){
+  
+  package_df <- installed.packages() %>% 
+    as.tibble()
+  
+  # remove base R packages and INLA
+  package_df <- package_df %>% 
+    filter(is.na(Priority)) %>% 
+    select(Package, Version)
+  # %>% 
+  # filter(!Package == "INLA")
+  return(package_df)
+  
+}
+
+
+package_list <- make_package_df()
 
