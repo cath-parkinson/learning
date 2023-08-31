6ea09d3822ff74af805d8a9973da38cbc9b3a9b2
Cath Parkinson <cparkinson15@gmail.com>
2023-08-31 11:15:19 +0100

    Incorporate Toms new chart and new data

--- R/practice/shiny/chatgpt-forecaster-demo/app.R
+++ R/practice/shiny/chatgpt-forecaster-demo/app.R
@@ -62,11 +62,15 @@ mm_highcharter_theme <- hc_theme(
 # Read data --------------------------------
 
 data <- readxl::read_xlsx("data.xlsx")
+categories <- c("AD-SPEND", "CREATIVE-SCORE", "MEDIA-MIX", "PROMOS", "PRICE", "COMPETITOR","ECONOMY", "SEASONALITY")
 
 sales <- data %>% 
   dplyr::filter(date > "2022-07-01") %>% 
   dplyr::mutate(weight = NA) %>% 
-  dplyr::mutate(weighted_value = NA)
+  dplyr::mutate(weighted_value = NA)%>%
+  # Ensure when we chart the data it does in the order specified here
+  dplyr::mutate(series_name = factor(series_name,
+                                     levels = c(categories, "PRODUCT-LAUNCH", "BASE")))
 
 # UI ------------------------
 
@@ -103,11 +107,21 @@ ui <- fluidPage(
     
     column(width = 8,
            fluidRow(column(width = 4,
-                           highchartOutput("chart2", height = "300px")),
+                           highchartOutput("chart2",
+                                           height = "480px"
+                                           )),
                     column(width = 8,
-                           highchartOutput("chart", height = "300px"))))
+                           highchartOutput("chart"
+                                           , height = "240px"
+                                           ),
+                           highchartOutput("chart3"
+                                           , height = "240px"
+                                           ))
+                    # ,
+                    # column(width = 8,
+                           # ))
            
-    )
+    )))
 )
 
 server <- function(input, output) {
@@ -124,8 +138,8 @@ server <- function(input, output) {
   
   # Initalise value - reactive values seems to be the most natural way to do this
   slider_vals <- reactiveValues()
-  # slider_vals$label <- c("MEDIA", "PROMOS", "PRICE", "COMPETITOR","ECONOMY", "SEASONALITY")
-  slider_vals$label <- c("AD-SPEND", "CREATIVE-SCORE", "MEDIA-MIX", "PROMOS", "PRICE", "COMPETITOR","ECONOMY", "SEASONALITY")
+  
+  slider_vals$label <- categories
   slider_vals$color <- c("primary", "primary", "primary", "warning", "warning", "warning", "danger", "danger")
   
   observe({
@@ -164,7 +178,6 @@ server <- function(input, output) {
                                         style = "pill",
                                         color = slider_vals$color[i],
                                         size = "sm"))
-               # h4(slider_vals$label[i])
         ),
         column(width = 8,
                div(id = "slider-row",
@@ -281,36 +294,36 @@ server <- function(input, output) {
   })
   
   output$chart <- renderHighchart({
-    
-    
+
+
     highcharter::highchart() %>%
       highcharter::hc_title(text = "Weekly Sales",
-                            align = "left") %>% 
-      highcharter::hc_add_series(data = sales_data$df %>% 
-                                   dplyr::group_by(model, date) %>% 
+                            align = "left") %>%
+      highcharter::hc_add_series(data = sales_data$df %>%
+                                   dplyr::group_by(model, date) %>%
                                    dplyr::summarise(weighted_value0 = sum(weighted_value0)) %>%
                                    dplyr::mutate(month = datetime_to_timestamp(date)
                                    ),
                                  name = "Benchmark",
-                                 hcaes(x = month, 
+                                 hcaes(x = month,
                                        y = weighted_value0),
                                  zoneAxis = "x",
-                                 zones = list(list(value = datetime_to_timestamp(as.Date("2023-07-01")), 
+                                 zones = list(list(value = datetime_to_timestamp(as.Date("2023-07-01")),
                                              dashStyle = "solid")),
                                  type = "line",
                                  dashStyle = "dash",
-                                 color = highlight1) %>% 
-      highcharter::hc_add_series(data = sales_data$df %>% 
-                                   dplyr::group_by(model, date) %>% 
+                                 color = highlight1) %>%
+      highcharter::hc_add_series(data = sales_data$df %>%
+                                   dplyr::group_by(model, date) %>%
                                    dplyr::summarise(weighted_value = sum(weighted_value)) %>%
                                    dplyr::mutate(month = datetime_to_timestamp(date)
                                    ),
                                  name = "My Scenario",
-                                 hcaes(x = month, 
+                                 hcaes(x = month,
                                        y = weighted_value),
-                                 type = "line", 
+                                 type = "line",
                                  dashStyle = "dash",
-                                 color = highlight2) %>% 
+                                 color = highlight2) %>%
       highcharter::hc_xAxis(type = "datetime",
                             # style = list(color = "#4F24EE"),
                             plotLines = list(list(color = highlight1,
@@ -322,8 +335,41 @@ server <- function(input, output) {
                                                                # rotation = 0,
                                                                # style = list(color = "#CBF6FF"))
                                                   ))) %>%
-      highcharter::hc_yAxis(min = 0) %>% 
+      highcharter::hc_yAxis(min = 0) %>%
       highcharter::hc_add_theme(mm_highcharter_theme)
+
+  })
+  
+  # Tom additions
+  
+  output$chart3 <- renderHighchart({
+    
+    category_data <- sales_data$df %>%
+      dplyr::filter(date > "2023-07-01") %>% 
+      dplyr::rename("Benchmark" = weighted_value0, "My Scenario" = weighted_value) %>%
+      dplyr::select(series_name, 'Benchmark', 'My Scenario') %>%
+      pivot_longer(cols = -series_name, names_to = "type") %>%
+      dplyr::group_by(series_name, type) %>%
+      dplyr::summarise(value = sum(value)) %>%
+      dplyr::filter(!series_name == "PRODUCT-LAUNCH") %>%
+      dplyr::filter(!series_name == "BASE") %>%
+      dplyr::mutate(series_name = gsub("-", " ", series_name))
+
+       
+    category_data %>%
+      highcharter::hchart(type = "column", 
+                          # The group is essential for allowing us to use a different color for each bar
+                          hcaes(x = series_name, y = value, group = type), color = c(highlight1, highlight2)) %>%
+      # Then this stacking = normal also formats the now colored series, groups correctly on the chart
+      # highcharter::hc_plotOptions(series = list(stacking = "normal"
+      #                                           # dataLabels = list(enabled = TRUE)
+      # )) %>% 
+      highcharter::hc_add_theme(mm_highcharter_theme) %>% 
+      hc_yAxis(title = "") %>% 
+      hc_xAxis(title = "") %>% 
+      highcharter::hc_title(text = "Sales Breakdown",
+                            align = "left") %>% 
+      hc_legend(enabled = T)
     
   })
   
